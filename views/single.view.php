eb160c3db05588ccec806d5e2998c5ebc827d754
Josh Click <yq272@nyu.edu>
2013-01-18 22:49:08 -0500

    it works fully now.. i think

--- views/single.view.php
+++ views/single.view.php
@@ -1,4 +1,5 @@
 <h2><?= $bet['title']; ?></h2>
-<div class="body"> <?= $bet['body']; ?> </div>
+<div class="name"> <?= $bet['name1']; ?> vs. <?= $bet['name2']; ?> </div>
+<div class="amount">For $<?= $bet['amount']; ?> </div>
 
 <a href="index.php">Back</a>

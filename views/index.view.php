b1abbecc34df8508e36a8b1422f0bf9da117982f
Ilaissa Romero <inr211@nyu.edu>
2013-01-19 04:33:37 -0500

    HTML
    Updated 4:33 *cries*

--- views/index.view.php
+++ views/index.view.php
@@ -13,29 +13,33 @@
 <div id = "main">
 <div id = "container">
 
-	<!--Logo Image I want it to be around W: 818 & H:200-->
-	<img src="logo.jpg" alt="Logo" width="818" height="200"> 
+	<!--Logo Image I want it to be around W: 818 & H:150-->
+	<img src="logo.jpg" alt="Logo" width="818" height="150"> 
 	
 	<!--Navigation--> 
 	<nav>
 		<ul>
-		<li><a href="index.wave.html"><div class = nav_text> HOME </div></a></li>
-		<li><a href="bet.html"><div class = nav_text> BET </div></a></li>
+		<li><a href="index.php"><div class = nav_text> HOME </div></a></li>
+		<li><a href="/admin/"><div class = nav_text> BET </div></a></li>
 		<li><a href="about.html"><div class = nav_text> ABOUT </div></a></li>	
 	</ul>
 	</nav>
 	
 	<!--Scrolling-->
 	<h1>The Wall of Bets</h1>
-		<a href="/admin/"> Make a Bet </a>
 		<!-- PHP -->
 		<?php foreach($bets as $bet) : ?>
        	<article class = "boxes">
         	<h2><?= $bet['title']; ?></h2>
-            <div class="bet-body">
-            	<?= $bet['team1']; ?> vs. <?= $bet['team2']; ?>: <?= $bet['name1']; ?> and <?= $bet['name2']; ?> for $<?= $bet['amount']; ?>
-                <a href="single.php?id=<?= $bet['id']; ?>"> Edit </a>
-            </div>
+            <div class="bet_body">
+            	<div class = "L_pic"> <img class = "noborder" src="Images/home.gif" alt="Picture" width="100" height="150"> </div>
+            	<div id = "cash"> - 500 <?= $bet['amount'];?> - </div>
+            	<div class = "R_pic"> <img class = "noborder" src="Images/home.gif" alt="Picture" width="100" height="150"> </div>
+            	<br /> <div class= "team">TEAM1<?= $bet['team1'];?> </div> <span>vs.</span> <div class="name">TEAM3<?= $bet['team2']; ?> </div>
+            	<br /> <div class= "name">DUDE1 <?= $bet['name1']; ?></div> <span>vs.</span><div class="name">DUDE2  <?= $bet['name2']; ?></div>
+                <!--<a href="single.php?id=<?= $bet['id']; ?>"> Edit </a>-->
+                
+			</div>
        </article>
 	<?php endforeach ?>
 	

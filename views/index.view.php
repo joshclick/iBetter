0d9b150083d01e55b09747536d07c608a32d175d
Josh Click <yq272@nyu.edu>
2013-01-19 05:57:14 -0500

    you silly face

--- views/index.view.php
+++ views/index.view.php
@@ -1,49 +1,28 @@
-<!DOCTYPE html>
-<html language = "en">
-<head> 
-	<!-- Page Information --> 
-	<meta name = "charset" content ="UTF-8">
-	<title> IBetter </title> 
-	<!-- <link rel="icon" type="image/png" href="Images/favicon.ico" />  LINK FOR THE FAVICON-->
-    <!-- <link rel="shortcut icon" href="Images/favicon.ico" /> Short Cut Icon -->
-    <link href="style.css" rel="stylesheet" type="text/css"> <!-- Style Sheet I haven't create yet -->
+<!--Logo Image I want it to be around W: 818 & H:150-->
+<img src="logo.jpg" alt="Logo" width="818" height="150"> 
 
-</head> 
-<body>
-<div id = "main">
-<div id = "container">
+<!--Navigation--> 
+<nav>
+	<ul>
+	<li><a href="index.php"><div class = nav_text> HOME </div></a></li>
+	<li><a href="/admin/"><div class = nav_text> BET </div></a></li>
+	<li><a href="about.html"><div class = nav_text> ABOUT </div></a></li>	
+</ul>
+</nav>
 
-	<!--Logo Image I want it to be around W: 818 & H:150-->
-	<img src="logo.jpg" alt="Logo" width="818" height="150"> 
-	
-	<!--Navigation--> 
-	<nav>
-		<ul>
-		<li><a href="index.php"><div class = nav_text> HOME </div></a></li>
-		<li><a href="/admin/"><div class = nav_text> BET </div></a></li>
-		<li><a href="about.html"><div class = nav_text> ABOUT </div></a></li>	
-	</ul>
-	</nav>
-	
-	<!--Scrolling-->
-	<h1>The Wall of Bets</h1>
-		<!-- PHP -->
-		<?php foreach($bets as $bet) : ?>
-       	<article class = "boxes">
-        	<h2><?= $bet['title']; ?></h2>
-            <div class="bet_body">
-            	<div class = "L_pic"> <img class = "noborder" src="Images/home.gif" alt="Picture" width="100" height="150"> </div>
-            	<div id = "cash"> - 500 <?= $bet['amount'];?> - </div>
-            	<div class = "R_pic"> <img class = "noborder" src="Images/home.gif" alt="Picture" width="100" height="150"> </div>
-            	<br /> <div class= "team">TEAM1<?= $bet['team1'];?> </div> <span>vs.</span> <div class="name">TEAM3<?= $bet['team2']; ?> </div>
-            	<br /> <div class= "name">DUDE1 <?= $bet['name1']; ?></div> <span>vs.</span><div class="name">DUDE2  <?= $bet['name2']; ?></div>
-                <!--<a href="single.php?id=<?= $bet['id']; ?>"> Edit </a>-->
-                
-			</div>
-       </article>
-	<?php endforeach ?>
-	
-</div> <!-- End of Containter  -->	
-</div> <!-- Main Page ID -- >
-</body>
-</html>
+<!--Scrolling-->
+<h1>The Wall of Bets</h1>
+	<!-- PHP -->
+	<?php foreach($bets as $bet) : ?>
+   	<article class = "boxes">
+        <div class="bet_body">
+        	<div class = "L_pic"> <img class = "noborder" src="Images/home.gif" alt="Picture" width="100" height="150"> </div>
+        	<div id = "cash">$<?= $bet['amount'];?></div>
+        	<div class = "R_pic"> <img class = "noborder" src="Images/home.gif" alt="Picture" width="100" height="150"> </div>
+        	<br /> <div class= "team"><?= $bet['team1'];?> </div> <span>vs.</span> <div class="name"><?= $bet['team2']; ?> </div>
+        	<br /> <div class= "name"><?= $bet['name1']; ?></div> <span>vs.</span><div class="name"><?= $bet['name2']; ?></div>
+            <!--<a href="single.php?id=<?= $bet['id']; ?>"> Edit </a>-->
+            
+		</div>
+   </article>
+<?php endforeach ?>
--- views/layout.php
+++ views/layout.php
@@ -2,11 +2,54 @@
 <html>
 <head>
 	<title>iBetter</title>
-	<link href="style.css" rel="stylesheet" type="text/css">
+	<link href="http://localhost/~josh/ibetter/views/style.css" rel="stylesheet" type="text/css">
 </head>
-<body>
+<body id="bg">
 	<div class="container">
 		<?php include($path); ?>
 	</div>
+
+	<script src="http://localhost/~josh/ibetter/js/jquery-1.9.0.min.js"></script>
+	<script src="http://localhost/~josh/ibetter/js/jquery.masonry.min.js"></script>
+	<script>
+	  	$(function(){
+		    $('.container').masonry({
+	  			itemSelector: '.bet_body',
+			});
+	  	});
+	</script>
 </body>
 </html>
+
+
+	<!--<style type="text/css">
+		.container { width: 90%; margin: auto; font-family:Helvetica, sans-serif;}
+		form li { list-style: none; }
+		form ul { padding: 0; }
+		form input[type=text], form textarea { margin-bottom: 1.5em; width: 100%; }
+		form textarea { height: 300px; }
+		label { display: block; }
+		.betBox {
+			position: relative;
+			  margin: 6px;
+			  float: left;
+		}
+		a {
+			color: black;
+			text-decoration: none;
+		}
+		.betBox h3 {
+		   position: absolute; 
+   			top: 10%; 
+   			left: 5%; 
+  			width: 90%; 
+  		}
+  		.betBox h3 span { 
+		   color: white; 
+		   line-height: 2.0em;
+		   letter-spacing: -1px;  
+		   background: rgb(0, 0, 0); /* fallback color */
+		   background: rgba(0, 0, 0, 0.7);
+		   padding: 10px; 
+		}
+	</style>-->
